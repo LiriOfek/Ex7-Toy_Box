68e0e84d13e56947d35da84f2ecc2954e154837a
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-07 13:51:16 +0300

    add main function for get box dimensions and print the calculations of the box parameters

--- toy_box_calculations/main.c
+++ toy_box_calculations/main.c
@@ -7,6 +7,15 @@ Purpose: This file create structure that represents box,
 \********************************************************/
 
 #define INITIAL_NUMBER (0)
+#define STRING_GET_BOX_WIDTH ("Enter box width: ")
+#define STRING_GET_BOX_LENGTH ("Enter box length: ")
+#define STRING_GET_BOX_HIGHT ("Enter box hight: ")
+#define GET_FLOAT ("%f")
+#define PRINT_BASE_FACE_AREA ("Base face area: %f\n")
+#define PRINT_SURFACE_AREA ("Surface area: %f\n")
+#define PRINT_BOX_VOLUME ("Box volume: %f\n")
+
+#include <stdio.h>
 
 struct box {
 	/*struct that contain the dimention of box*/
@@ -119,4 +128,56 @@ float calculate_box_volume(struct box toy_box) {
 	box_volume = base_face_area * toy_box.hight;
 
 	return box_volume;
+}
+
+void main() {
+	/********************************************************\
+	* Function name - main
+	*
+	* Function Purpose - get dimensions from user, create box structure
+	*					 and fill it with the dimensions,
+	*					 print the base face area, surface area, and the volume
+	*
+	*
+	* Parameters - no Input/Output parameters
+	*
+	* Return Value - there isn't return value
+	*
+	* Side Effects - this function has no side effects
+	*
+	* Semantics - get dimensions from user, create box structure
+	*			  and fill it with the dimensions,
+	*			  call functions that calculate the base face area,
+	*			  surface area, and the volume
+	*
+	* Author - Liri
+	\********************************************************/
+	float box_width = INITIAL_NUMBER;
+	float box_length = INITIAL_NUMBER;
+	float box_hight = INITIAL_NUMBER;
+	struct box toy_box;
+	float base_face_area = INITIAL_NUMBER;
+	float surface_area = INITIAL_NUMBER;
+	float box_volume = INITIAL_NUMBER;
+
+	/*get dimensions drom user*/
+	printf(STRING_GET_BOX_WIDTH);
+	scanf_s(GET_FLOAT, &box_width);
+	printf(STRING_GET_BOX_LENGTH);
+	scanf_s(GET_FLOAT, &box_length);
+	printf(STRING_GET_BOX_HIGHT);
+	scanf_s(GET_FLOAT, &box_hight);
+
+	toy_box = make_box(box_width, box_length, box_hight);
+
+	/*calclation of box parameters*/
+
+	base_face_area = calculate_face_area(toy_box.width, toy_box.length);
+	printf(PRINT_BASE_FACE_AREA, base_face_area);
+
+	surface_area = calculate_surface_area(toy_box);
+	printf(PRINT_SURFACE_AREA, surface_area);
+
+	box_volume = calculate_box_volume(toy_box);
+	printf(PRINT_BOX_VOLUME, box_volume);
 }
