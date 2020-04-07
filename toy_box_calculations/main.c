379692e8bbf18c5451e3ec43536b905b2815d2eb
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-07 13:26:44 +0300

    add main file with documentation

---
+++ toy_box_calculations/main.c
@@ -0,0 +1,7 @@
+/********************************************************\
+File Name: main.c
+Author: Liri (5/4/2020)
+Purpose: This file create structure that represents box,
+		 calculate base face area of box, surface area,
+		 and volume of the box
+\********************************************************/
