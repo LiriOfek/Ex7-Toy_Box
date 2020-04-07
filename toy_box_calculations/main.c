d696f4f7b15283312160349829a6977add0b969a
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-07 13:29:13 +0300

    add function that make box struct with given parameters

--- toy_box_calculations/main.c
+++ toy_box_calculations/main.c
@@ -12,3 +12,31 @@ struct box {
 	float length;
 	float hight;
 };
+
+struct box make_box(float witdth, float length, float hight) {
+	/********************************************************\
+	* Function name - make_box
+	*
+	* Function Purpose - make box struct with the given dimensions
+	*
+	* Parameters - IN float witdth - box width
+	*			   IN float length - box length
+	*			   IN float hight - box hight
+	*			   OUT struct box toy_box - struct of the box with
+	*								         the given dimentions
+	*
+	* Return Value - struct of the box with the given dimentions
+	*
+	* Side Effects - this function has no side effects
+	*
+	* Semantics - this function create box struct, containing the given dimensions
+	*
+	* Author - Liri
+	\********************************************************/
+	struct box toy_box;
+
+	toy_box.width = witdth;
+	toy_box.length = length;
+	toy_box.hight = hight;
+	return toy_box;
+}
