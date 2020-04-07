13a964673382309c214103bbff01ca5ef8d8dd82
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-07 13:27:45 +0300

    add box struct, containing the dimanions of the box

--- toy_box_calculations/main.c
+++ toy_box_calculations/main.c
@@ -5,3 +5,10 @@ Purpose: This file create structure that represents box,
 		 calculate base face area of box, surface area,
 		 and volume of the box
 \********************************************************/
+
+struct box {
+	/*struct that contain the dimention of box*/
+	float width;
+	float length;
+	float hight;
+};
